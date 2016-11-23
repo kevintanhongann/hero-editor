3ef186c616febef2468c21b19c14b558fa1e8630
kevintan <kevintanhongann@gmail.com>
2016-11-23 23:43:08 +0800

    simulate slow response for get heroes

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -10,6 +10,12 @@ export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(resolve, 2000)) // delay 2 seconds
+      .then(() => this.getHeroes());
+  }
 }
 
 // TypeScript sees the @Injectable() decorator and emits metadata about our service, metadata that Angular may need to inject other dependencies into this service.
