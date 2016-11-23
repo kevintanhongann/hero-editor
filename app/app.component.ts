c2e4dae0508d8963cf5523f83b8c5c51fcc21f47
kevintan <kevintanhongann@gmail.com>
2016-11-23 11:39:33 +0800

    inject hero mock service and promises

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,10 @@ import {HeroService} from "./hero.service";
 
 
 @Component({
-  providers: [HeroService],
   // The providers array tells Angular to create a fresh instance of the HeroService when it creates a new AppComponent. The AppComponent can use that service to get heroes and so can every child component of its component tree.
+  providers: [HeroService],
 
     selector: 'my-app',
-  constructor(private heroService: HeroService) { },
   template: `
   <h2>My Heroes</h2>
   <!-- The leading asterisk (*) in front of ngFor is a critical part of this syntax. -->
@@ -81,12 +80,14 @@ export class AppComponent implements OnInit{
   heroes: Hero[];
   selectedHero: Hero;
 
+  constructor(private heroService: HeroService) { }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -3,13 +3,16 @@
  */
 import { Injectable } from '@angular/core';
 import {HEROES} from "./mock-heroes";
+import {Hero} from "./hero";
 
 @Injectable()
 export class HeroService {
-  getHeroes(): void {
-    return HEROES;
-  } // stub
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
 }
 
 // TypeScript sees the @Injectable() decorator and emits metadata about our service, metadata that Angular may need to inject other dependencies into this service.
+
+// We're still mocking the data. We're simulating the behavior of an ultra-fast, zero-latency server, by returning an immediately resolved Promise with our mock heroes as the result.
 
